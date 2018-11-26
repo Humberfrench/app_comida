10113f3898a779e1714f1000f3455ce619f6dad3
camila assis <cabukater@gmail.com>
2018-11-26 16:25:40 -0200

    binding details restaurants in tab detais

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {ActivatedRoute} from '@angular/router'
+
+import{ RestaurantsService } from '../restaurants/restaurants.service'
+import { Restaurant } from '../restaurants/restaurant/restaurant.model'
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-detail.component.css']
 })
 export class RestaurantDetailComponent implements OnInit {
+  restaurant : Restaurant
 
-  constructor() { }
+  constructor(
+    private restaurantsService : RestaurantsService,
+    private route: ActivatedRoute
+  ) { 
+
+  }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+     .subscribe(
+      restaurant => this.restaurant = this.restaurant = restaurant
+    )
   }
 
 }
